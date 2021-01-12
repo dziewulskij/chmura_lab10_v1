6418925351488b7fb99b7fb687c1f84be702a511
Jarosław Dziewulski <32748048+dziewulskij@users.noreply.github.com>
2021-01-12 16:46:12 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.lang.*;
 public class Main{
 	public static void main(String[] args){
 		while(true){
-			System.out.println("Version 1");
+			System.out.println("Kontener w wersji 1");
 			wait(5000);
 		}
 	}
